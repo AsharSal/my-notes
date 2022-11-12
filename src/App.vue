370edce05facf38a7360a9feea7f4db1059352e1
AsharSal <asharsaleem4@gmail.com>
2022-11-12 20:29:17 +0500

    Vue3 My notes app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<script setup>
+/*
+  imports
+*/
+  import NavBar from '@/components/Layout/NavBar.vue'
+</script>
+<template>
+  <NavBar></NavBar>
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
+</template>
+
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/views/ViewStats.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="stats">
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Stat</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Number of Notes</td>
+          <td>{{ storeNotes.totalNotesCount }}</td>
+        </tr>
+        <tr>
+          <td>Number of Characters (of all notes)</td>
+          <td>{{ storeNotes.totalCharactersCount }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <input
+      v-model="loveMyNotes"
+      class="input"
+      type="text"
+      placeholder="Do you love MyNotes??"
+      v-autofocus
+    />
+  </div>
+</template>
+
+<script setup>
+/*
+  imports
+*/
+  import { ref } from 'vue'
+  import { useStoreNotes } from '@/stores/storeNotes'
+  import { vAutofocus } from '@/directives/vAutofocus'
+  import { useWatchCharacters } from '@/use/useWatchCharacters'
+/*
+  store
+*/
+  const storeNotes = useStoreNotes()
+/*
+  love MyNotes
+*/
+  const loveMyNotes = ref('')
+  useWatchCharacters(loveMyNotes, 50)
+</script>
